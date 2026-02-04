95de0bb15bb64e35217f3b0953454098869e09ce
Lawrence Arryl Lopez <160656561+arryllopez@users.noreply.github.com>
2026-02-04 16:36:16 -0500

    net http allows for named constants

--- gin-quickstart/main.go
+++ gin-quickstart/main.go
@@ -1,13 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func main() { 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+
 	router.Run() // listen and serve on 0.0.0.0:8080
 }
