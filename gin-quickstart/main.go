07936033b9d4580de21943eb007a17e5f9ab82b4
Lawrence Arryl Lopez <160656561+arryllopez@users.noreply.github.com>
2026-02-05 20:44:28 -0500

    token bucket algorithm implementation

--- gin-quickstart/main.go
+++ gin-quickstart/main.go
@@ -1,13 +1,108 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+// implementing custom middleware
+// this Logging middleware will be where the algorithm that has rate limiting via token bucket algo
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		type TokenBucket struct {
+			tokens       int
+			capacity     int
+			refillRate   int
+			stopRefiller chan struct{} //signal to stop refilling
+			mu           sync.Mutex    // handling race conditions (two processes trying to access tokens simultaneously)
+		}
+
+		func NewTokenBucket(capacity, tokensPerInterval int, refillRate time.Duration) *TokenBucket) {
+			tb := &TokenBucket{
+				capacity:     capacity,
+				refillRate:   refillRate,
+				stopRefiller: make(chan struct{})
+			}
+			go tb.tokens = capacity // start with a full bucket
+			return tb
+		}
+
+		func (tb *TokenBucket) refillTokens(tokensPerInterval int) {
+			// ticker is a great way to do something repeatedly to know more
+			// check this out - https://gobyexample.com/tickers
+			ticker := time.NewTicker(tb.refillRate)
+			defer ticker.Stop()
+
+			for {
+				select {
+				case <-ticker.C:
+					// handle race conditions
+					tb.mu.Lock()
+					if tb.tokens+tokensPerInterval <= tb.capacity {
+						// if we won't exceed the capacity add tokensPerInterval
+						// tokens into our bucket
+						tb.tokens += tokensPerInterval
+					} else {
+						// as we cant add more than capacity tokens, set
+						// current tokens to bucket's capacity
+						tb.tokens = tb.capacity
+					}
+					tb.mu.Unlock()
+				case <-tb.stopRefiller:
+					// let's stop refilling
+					return
+				}
+			}
+		}
+
+		func (tb *TokenBucket) TakeTokens() bool {
+			// handle race conditions
+			tb.mu.Lock()
+			defer tb.mu.Unlock()
+
+			// if there are tokens available in the bucket, we take one out
+			// in this case request goes through, thus we return true.
+			if tb.tokens > 0 {
+				tb.tokens--
+				return true
+			}
+			// in the case where tokens are unavailable, this request won't
+			// go through, so we return false
+			return false
+		}
+
+		func (tb *TokenBucket) StopRefiller() {
+			// close the channel
+			close(tb.stopRefiller)
+		}
+	}
+}
+
 func main() {
 	router := gin.Default()
+	router.Use(Logger()) // applying the middleware
+
+	// LoggerWithFormatter middleware will write the logs to gin.DefaultWriter
+	// By default gin.DefaultWriter = os.Stdout
+	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		// your custom format
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+	router.Use(gin.Recovery())
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -31,6 +126,12 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{
 			"status": "purchase successful",
 		})
+	})
+
+	router.GET("/test", func(c *gin.Context) {
+		example := c.MustGet("example").(string)
+		// it would print: "12345"
+		log.Println(example)
 	})
 
 	router.Run() // listen and serve on 0.0.0.0:8080
